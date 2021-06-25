2804fa2c74defe90e7814d873e38855a205bfb66
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-06-25 15:57:21 -0600

    Updated GUI

--- src/gui.c
+++ src/gui.c
@@ -1,1 +1,16 @@
+// A fancy interface
+
 #include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+  char userInput[100];
+
+  printf("My fancy interface will repeat your input..\n");
+  fgets(userInput, 100, stdin);
+  printf("Repeating...\n");
+  printf("%s\n", &userInput);
+  
+}
+
