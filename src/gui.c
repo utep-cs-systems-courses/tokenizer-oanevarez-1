dd6b462ff82814385929c024f92df70fdfbe45f5
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-06 12:30:08 -0600

    Updated UI : referenced project 0 UI

--- src/gui.c
+++ src/gui.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
 
 void main ()
 {
@@ -11,6 +13,36 @@ void main ()
   fgets(userInput, 100, stdin);
   printf("Repeating...\n");
   printf("%s\n", &userInput);
+
+  List *history = init_history();
+  char **tokens = tokenize(userInput);
+
+  while(1){
+    fputs("History enter h:, Tokenize enter t:, Quit enter q:\n", stdout);
+    fflush(stdout);
+    int c;
+    while((c = getchar()) == '\n');
+    if (c == EOF)
+      goto done;
+
+    switch(c){
+    case 't':
+      print_tokens(tokens);
+      break;
+    case 'h':
+      printf("--HISTORY--\n");
+      add_history(history, tokens);
+      print_history(history);
+      free_history(history);
+      break;
+    case 'q':
+      puts("Bye!");
+      goto done;
+    }
+  }
+
+ done:
+  return 0;
   
 }
 
